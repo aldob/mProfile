7f9286b9c6daa79129f4e2c35d772f45e3be518c
Aldo Saleh Bader <40291899+aldob@users.noreply.github.com>
2019-12-04 13:10:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='mprofile',
       version='0.1',
-      description='nucleotid-resolution mutation calling',
+      description='Mutation calling and translocation mapping or targeted sequencing data',
       url='https://github.com/aldob/mProfile',
       packages=find_packages(),
-      author='Aldo Bader',
+      author='Aldo Saleh Bader',
       zip_safe=False)
