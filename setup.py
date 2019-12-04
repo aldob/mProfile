70abf92535ed7b1e21efd68487b1d18c27002ed8
Aldo Saleh Bader <40291899+aldob@users.noreply.github.com>
2019-12-04 13:09:51 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='callmut',
+setup(name='mprofile',
       version='0.1',
       description='nucleotid-resolution mutation calling',
       url='https://github.com/aldob/mProfile',
-      packages=['callmut'],
+      packages=find_packages(),
       author='Aldo Bader',
       zip_safe=False)
