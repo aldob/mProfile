b4d0435cc28353a434b6d7651f2d61aa0076665e
Aldo Saleh Bader <40291899+aldob@users.noreply.github.com>
2019-12-04 10:36:48 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='callmut',
+      version='0.1',
+      description='nucleotid-resolution mutation calling',
+      url='https://github.com/aldob/mProfile',
+      packages=['callmut'],
+      author='Aldo Bader',
+      zip_safe=False)
