fcf54260ad5d2e98058118502bfdc86e7298389c
Aldo Saleh Bader <40291899+aldob@users.noreply.github.com>
2019-12-06 13:14:00 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='mprofile',
-      version='0.1',
-      description='Mutation calling and translocation mapping or targeted sequencing data',
-      url='https://github.com/aldob/mProfile',
-      packages=find_packages(),
-      author='Aldo Saleh Bader',
-      zip_safe=False)
