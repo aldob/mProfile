ebaa96cbfa00cc68dfed03572f3da0ceebd66ae6
Aldo Saleh Bader <40291899+aldob@users.noreply.github.com>
2020-07-08 17:42:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='mProfile',
       version='1.1',
       description='nucleotid-resolution mutation calling',
       url='https://github.com/aldob/mProfile',
+      download_url = 'https://github.com/aldob/mProfile/archive/v1.1.tar.gz',
       packages=find_packages(),
       author='Aldo Bader',
       entry_points={
