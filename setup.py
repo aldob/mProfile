1a3b205a3e012726e1d21478cda1c8522eb331f4
Aldo Saleh Bader <40291899+aldob@users.noreply.github.com>
2021-08-02 13:38:20 +0100

    v1.2 update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='mProfile',
-      version='1.1',
-      description='nucleotid-resolution mutation calling',
+setup(name='mProfile-mut',
+      version='1.2',
+      description='nucleotide-resolution mutation calling',
       url='https://github.com/aldob/mProfile',
-      download_url = 'https://github.com/aldob/mProfile/archive/v1.1.tar.gz',
+      download_url = 'https://github.com/aldob/mProfile/archive/v1.2.tar.gz',
       packages=find_packages(),
-      author='Aldo Bader',
+      author='Aldo Saleh Bader',
       entry_points={
         'console_scripts': [
             'callMUT = mProfile.callMUT:main',
